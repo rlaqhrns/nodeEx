a802c3909889e3d8ca3c17b692224d56d6172fc0
rlaqhrns <63628096+rlaqhrns@users.noreply.github.com>
2021-01-27 22:19:14 +0900

    react0127

--- 0124/react-task-tracker/src/App.js
+++ 0124/react-task-tracker/src/App.js
@@ -1,25 +1,32 @@
-import './App.css';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
-import {useState} from 'react';
-
+import './App.css'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {id:1,text:"병원 예약",day:"2020-11-12",reminder:true},
-    {id:2,text:"학교 미팅", day:"2020-11-13",reminder:true},
-    {id:3,text:"물건 구매",day:"2020-12-19",reminder:false},
-    ]);
-  const deleteTask = (id) => {
-    console.log("삭제 !!!",id);
-  }
-  return (
-    <div className ="App">
-      <Header title="React" />
-      <h1>안녕하세요</h1>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+        { id: 1, text: '병원 예약', day: '2020-11-12', reminder: true },
+        { id: 2, text: '학교 미팅', day: '2020-11-13', reminder: true },
+        { id: 3, text: '물건 구매', day: '2020-12-19', reminder: false },
+    ])
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+
+        console.log('삭제 !!!', tasks)
+    }
+
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+        console.log(tasks)
+    }
+
+    return (
+        <div className='App'>
+            <Header title='React' />
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : '태스크가 없다'}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- 0124/react-task-tracker/src/components/Tasks.js
+++ 0124/react-task-tracker/src/components/Tasks.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import Task from './Task';
-export default function Tasks({tasks,onDelete}){
-  
-  return (
-    //setTasks([...tasks,{}])
-    <div>
-      {tasks.map( (task) => (
-        <Task key={task.id} task={task} onDelete={onDelete}>
-          {' '}
-        </Task>
-      ))}
-      
-    </div>
-  )
+import React from 'react'
+import Task from './Task'
+export default function Tasks({ tasks, onDelete, onToggle }) {
+    return (
+        //setTasks([...tasks,{}])
+        <div>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}></Task>
+            ))}
+        </div>
+    )
 }
